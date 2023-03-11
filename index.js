9556f48628ad3698752e846558c86e990df8c505
owens <65927654+Owens94819@users.noreply.github.com>
2023-03-11 14:49:06 +0100

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,31 @@ https.request(options, (req) => {
     //res.setHeader('content-type',req.headers['content-type'])
     //req.pipe(res);
     res.send("OK_1");
-    
+   // const inputStream = fs.createReadStream('path/to/input/video.mp4');
+//const outputStream = fs.createWriteStream('path/to/output/compressed.mp4');
+
+const crfValue = 28;
+const videoBitrate = '500k';
+const audioBitrate = '128k';
+
+ffmpeg(req)
+  //.output(outputStream)
+  .videoCodec('libx264')
+  .size('50%')
+  .videoFilters('scale=trunc(iw/2)*2:trunc(ih/2)*2')
+  .audioCodec('aac')
+  .audioBitrate(audioBitrate)
+  .outputOptions([
+    `-b:v ${videoBitrate}`,
+    `-crf ${crfValue}`
+  ])
+  .on('error', function(err) {
+    console.log('An error occurred: ' + err.message);
+  })
+  .on('end', function() {
+    console.log('Compression finished');
+  })
+  .run();
 })
 .on('error', (error) => {
   console.error(error);
