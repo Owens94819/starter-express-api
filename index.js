83bdf70253d907d11e0339348988cd52bddf619b
owens <65927654+Owens94819@users.noreply.github.com>
2023-03-11 15:27:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -53,24 +53,32 @@ const crfValue = 28;
 const videoBitrate = '500k';
 const audioBitrate = '128k';
 
-ffmpeg(req)
-  .output(res)
-  .videoCodec('libx264')
-  .size('50%')
+ffmpeg()
+  //.output(res)
+  //.videoCodec('libx264')
+ // .size('50%')
+   .input(req)
+    .videoCodec('libx264')
+    .audioCodec('aac')
+    .outputFormat('mp4')
+    .outputOptions([
+        '-movflags frag_keyframe+empty_moov'
+    ])
+    .pipe(outStream, { end: true });
   //.videoFilters('scale=trunc(iw/2)*2:trunc(ih/2)*2')
-  .audioCodec('aac')
-  .audioBitrate(audioBitrate)
-  .outputOptions([
-    `-b:v ${videoBitrate}`,
-    `-crf ${crfValue}`
-  ])
+  //.audioCodec('aac')
+//  .audioBitrate(audioBitrate)
+  //.outputOptions([
+  //  `-b:v ${videoBitrate}`,
+    //`-crf ${crfValue}`
+ // ])
   .on('error', function(err) {
     console.log('An error occurred: ' + err.message);
   })
   .on('end', function() {
     console.log('Compression finished');
   })
-  .run();
+  //.run();
 //res.send("OK_3");
 })
 .on('error', (error) => {
