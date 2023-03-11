8d37f6e82f92a98d9aef2ed397eb53c20cf5966f
owens <65927654+Owens94819@users.noreply.github.com>
2023-03-11 11:06:15 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,53 @@
+
+const ffmpegLib = require('@ffmpeg-installer/ffmpeg');
+
+console.log(ffmpegLib);
+
+const ffmpeg = require('fluent-ffmpeg');
+ffmpeg.setFfmpegPath(ffmpegLib.path);
+
+//const ffmpegStatic = require('ffmpeg-static');
+//const ffmpeg = require('fluent-ffmpeg');
+// Tell fluent-ffmpeg where it can find FFmpeg
+//ffmpeg.setFfmpegPath(ffmpegStatic);
+
+
+
+const https = require('https');
+const express = require('express');
+const app=express();
+const server = app.listen(process.env.PORT || 12345, () => {
+    var port = server.address().port;
+    console.log(`http://localhost:${port}\n-----------`);
+  });
+
+  const user="owens94819";
+  const repo="owens94819";
+  const branch="main";
+  const path="JavaScript VUE TUTORIAL in 60 Seconds_ ----‍-- _shorts(480P).mp4";
+  
+ app.use("/upload",function(req,res) {
+    console.log("we got some data");
+    res.send("OK");
+  });
+  
+  app.use("/compress",function(req,res) {
+    // body...
+ const raw=`https://raw.githubusercontent.com/${user}/${repo}/${branch}/${path}`
+ const options = raw;
+
+const req = https.request(options, (req) => {
+  console.log(`statusCode: ${req.statusCode}`);
+    //res.status(req.statusCode);
+    //res.setHeader('content-type',req.headers['content-type'])
+    //req.pipe(res);
+    console.log(ffmpeg)
+});
+
+req.on('error', (error) => {
+  console.error(error);
+});
+
+req.end();
+
+  })
